bee82b5d750dbc58273b19f60516b63b829176e6
Vinicius Brandao <viniciusbrandao@vb4web.com>
2024-05-16 22:32:06 -0300

    feat: create Transactions entity

---
+++ src/main/java/com/kipper/picpay/domain/transaction/Transactions.java
@@ -0,0 +1,34 @@
+package com.kipper.picpay.domain.transaction;
+
+import com.kipper.picpay.domain.user.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity(name = "transactions")
+@Table(name = "transactions")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Transactions {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private BigDecimal amount;
+
+    @ManyToOne
+    @JoinColumn(name = "sender_id")
+    private User sender;
+
+    @ManyToOne
+    @JoinColumn(name = "receiver_id")
+    private User receiver;
+
+    private LocalDateTime timeStamp;
+}
